1fb02decdbd7555689135c7cc3e9bf5338a5e41d
Jean Couteau <jean.couteau@gmail.com>
2015-12-16 10:40:44 +0100

    Clean code

--- merohc-rest/src/main/java/org/chorem/merohc/services/CompanyController.java
+++ merohc-rest/src/main/java/org/chorem/merohc/services/CompanyController.java
@@ -42,7 +42,7 @@ public class CompanyController {
 
     @RequestMapping(value="/v1/company/add", method= RequestMethod.PUT)
     public CompanyDTO addCompany(@RequestParam(value="name") String name,
-                              @RequestParam(value="type", required = false) String type) {
+                                 @RequestParam(value="type", required = false) String type) {
         Company companyToStore = companyDao.create();
         companyToStore.setName(name);
         persistenceContext.commit();
@@ -74,8 +74,8 @@ public class CompanyController {
 
     @RequestMapping(value="/v1/company", method= RequestMethod.POST)
     public CompanyDTO editCompany(@RequestParam(value="id") String id,
-                               @RequestParam(value="name") String name,
-                               @RequestParam(value="type", required = false) String type) {
+                                  @RequestParam(value="name") String name,
+                                  @RequestParam(value="type", required = false) String type) {
 
         Company company = companyDao.forTopiaIdEquals(id).findAny();
         //FIXME JC151211 - Deal with TopiaNoResultException
